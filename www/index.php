955c486c9b540e7d5b8f3467e8c31149ca9589e1
twenty1-23 <malacrichard@gmail.com>
2022-07-21 13:37:44 -0400

    bruh

--- www/index.php
+++ www/index.php
@@ -1,22 +1,61 @@
 <?php
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {}
+    
+    define("IS_DEBUG", $_SERVER["HTTP_HOST"] == "localhost" ? true : false);
+    
+    $firstname = $lastname = $email = $message = "";
+    $firstnameError = $lastnameError = $emailError = $messageError = "";
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (IS_DEBUG) {
+            echo "POST";
+        }
+        $firstname = isset($_POST['firstname']) ? checkInput($_POST("firstname")) :"";
+        $lastname = isset($_POST['lastname']) ? checkInput($_POST("lastname")) :"";
+        $email = isset($_POST['email']) ? checkInput($_POST("email")) :"";
+        if (!checkEmail($email)) {
+            $emailError = "Please check your email address";
+        }
+        $message = isset($_POST['message']) ? checkInput($_POST("message")) :"";
+
+    }
     else {
         echo "No POST";
+    }
+
+    function checkInput($input) {
+        $input = trim($input);
+        $input = stripcslashes($input);
+        $input = htmlspecialchars($input);
+            if (IS_DEBUG) {
+                echo $input;
+            }
+        return $input;
+            
+    }
+
+    function checkEmail($email) {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 ?>
 
 <!doctype html>
 <html lang="fr">
 
-<?php require "./element/head.php"; ?>
+<?php require "head.php"; ?>
 
 <body>
     <div id="formulaire">
-        <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-            <input type="text" placeholder="firstname" name="firstname" required>
-            <input type="text" placeholder="lastname" name="lastname" required>
-            <input type="email" placeholder="exmemple@email.com" required>
-            <textarea name="message"  placeholder="message(optional)" id="text-area" cols="30" rows="10" style="display: block; margin: auto; width: 250px;"></textarea>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+            <input type="text" placeholder="firstname" name="firstname" value="<?php echo $firstname ?>" required>
+            <input type="text" placeholder="lastname" name="lastname" value="<?php echo $lastname ?>" required>
+            <input type="email" placeholder="exmemple@email.com" name="email" value="<?php echo $email ?>" required>
+
+            <?php
+                if($emailError !== ""){
+                    echo "<p class='error>" . $emailError . "</p>";
+                }
+            ?>
+            <textarea name="message" placeholder="message(optional)" id="text-area" cols="30" rows="10" style="display: block; margin: auto; width: 250px;"><?php echo $message ?></textarea>
             <!-- <input type="password" placeholder="mot de passe" required> -->
             <!-- <div id="select"> 
             <select name="date">
